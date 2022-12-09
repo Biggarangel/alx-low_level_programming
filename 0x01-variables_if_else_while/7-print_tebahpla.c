2b809dce5dcf2a3f71c8b275c6a5f88d35488518
Biggarangel <charlize.biggar@gmail.com>
2022-12-09 13:39:37 -0800

    Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+ #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main -  lowercase alphabet in reverse
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c = 'z';
+
+	while (c >= 'a')
+	{
+		putchar(c);
+		c--;
+	}
+	putchar('\n');
+	return (0);
+}
